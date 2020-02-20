13f5f0cbec6845ad4c25886bfeb076e6e4e2e258
Aditya Prakash <adityaprakash.bobby@gmail.com>
2020-02-20 12:59:39 +0530

    add lambda to map websocket connId and textract jobId

---
+++ lambda_save_jobid_to_dynamo.py
@@ -0,0 +1,32 @@
+import json
+import boto3
+
+def lambda_handler(event, context):
+    print(event)
+    # TODO:
+    # Save execution job id mapped to the connection id of the webhook connection
+    
+    dynamo_client = boto3.client('dynamodb')
+    
+    body = json.loads(event['body'])
+    job_id = body['jobid']
+    connection_id = event['requestContext']['connectionId']
+    
+    # Create a connectionId and jobId map in dynamoDB
+    response = dynamo_client.put_item(
+        Item={
+            'jobId': {
+                'S': job_id,
+            },
+            'connectionId': {
+                'S': connection_id,
+            }
+        },
+        ReturnConsumedCapacity='TOTAL',
+        TableName='Music',
+    )
+    
+    return {
+        'statusCode': 200,
+        'body': json.dumps("Done!")
+    }
